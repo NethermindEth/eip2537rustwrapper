16b27760b3105fda60818b4b4c92c2c251417ab9
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-05-19 20:43:40 +0300

    initial

---
+++ builder/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git submodule update —recursive —init
+cd eip1962
+cp eip2537_header.h ../../
+cargo build --release --no-default-features --features=eip_2357_c_api
+cp target/release/libeth_pairings.a ../../libs
