8b3aed20571fbbc980f1998dc7330b66000b730e
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-05-20 14:52:07 +0300

    add linker flag

--- builder/build.sh
+++ builder/build.sh
@@ -2,5 +2,5 @@
 git submodule update —recursive —init
 cd eip1962
 cp eip2537_header.h ../../
-cargo build --release --no-default-features --features=eip_2357_c_api
+RUSTFLAGS="-C link-args=-lc" cargo build --release --no-default-features --features=eip_2357_c_api
 cp target/release/libeth_pairings.a ../../libs
