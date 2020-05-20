308be75d5c855871e9a8badd759f570e77ad8661
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-05-20 14:54:45 +0300

    fix dashes

--- builder/build.sh
+++ builder/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git submodule update —recursive —init
+git submodule update --recursive --init
 cd eip1962
 cp eip2537_header.h ../../
 RUSTFLAGS="-C link-args=-lc" cargo build --release --no-default-features --features=eip_2357_c_api
